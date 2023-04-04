73f9d35944268d65249d827986e1db58eb4127c5
qweerbecouse <kirill.com143@gmail.com>
2023-04-04 21:19:33 +0300

    init vectorVoid

--- main.c
+++ main.c
@@ -1,56 +1,14 @@
-#include "mother/data_structures/vector/vector.h"
+#include <stdio.h>
 
-void test_atVector_notEmptyVector() {
-    Vector v = createVector(2);
-    pushBack(&v, 10);
-    pushBack(&v, 20);
-
-    int *element = atVector(&v, 1);
-    assert(*element == 20);
-
-    deleteVector(&v);
-}
-
-void test_atVector_requestToLastElement() {
-    Vector v = createVector(2);
-    pushBack(&v, 10);
-    pushBack(&v, 20);
-
-    int *element = atVector(&v, v.size - 1);
-    assert(*element == 20);
-
-    deleteVector(&v);
-}
-
-void test_back_oneElementInVector() {
-    Vector v = createVector(1);
-    pushBack(&v, 10);
-
-    int *element = back(&v);
-    assert(*element == 10);
-
-    deleteVector(&v);
-}
-
-void test_front_oneElementInVector() {
-    Vector v = createVector(1);
-    pushBack(&v, 10);
-
-    int *element = front(&v);
-    assert(*element == 10);
-
-    deleteVector(&v);
-}
-
-void test() {
-    test_atVector_notEmptyVector();
-    test_atVector_requestToLastElement();
-    test_back_oneElementInVector();
-    test_front_oneElementInVector();
-}
+typedef struct VectorVoid {
+    void *data;
+    size_t size;
+    size_t capacity;
+    size_t baseTypeSize;
+} VectorVoid;
 
 int main() {
-    test();
+    VectorVoid v = {NULL, 0, 0, sizeof(int)};
 
     return 0;
 }
